195a0dc061fb3fc96fd00998ac3a8d406a851ac9
chrillegee <chrilleg123@gmail.com>
2026-04-28 20:40:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Uppdatera versionsnumret nedan varje gång du laddar upp ny index.html
 // Det tvingar alla användare att hämta ny version automatiskt
-const VERSION = 'matdagbok-v5';
+const VERSION = 'matdagbok-v6';
 const STATIC = [
   './manifest.json',
   './icon-192.svg',
